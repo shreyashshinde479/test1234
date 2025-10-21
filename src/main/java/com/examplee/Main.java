ebacf52623ae1e52305b8ede5d3d1e59881d4570
shreyashshinde479 <shindeshreyash479@gmial.com>
2025-10-21 21:27:08 +0530

    emo added

--- src/main/java/com/examplee/Main.java
+++ src/main/java/com/examplee/Main.java
@@ -3,5 +3,7 @@ package com.examplee;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        emo.main(args);
     }
+
 }
---
+++ src/main/java/com/examplee/emo.java
@@ -0,0 +1,12 @@
+package com.examplee;
+
+public class emo {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'main'");
+    }
+    
+        System.out.println("emo");
+    
+}
